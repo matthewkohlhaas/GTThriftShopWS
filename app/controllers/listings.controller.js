49c5001e5f8b206f8cda85c5cd7c3b188a0d02a0
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-02-01 18:17:19 -0500

    refactor authentication utils import variable name

--- app/controllers/listings.controller.js
+++ app/controllers/listings.controller.js
@@ -1,8 +1,8 @@
-var verification = require('../utils/authentication.utils');
+var authentication = require('../utils/authentication.utils');
 var Listing = require('../models/listing.model');
 
 exports.list = function (req, res, next) {
-    if (!verification.authenticateToken(req)) {
+    if (!authentication.authenticateToken(req)) {
         return res.status(401).send('unauthorized');
     } else {
         Listing.find({}, function (err, listings) {
