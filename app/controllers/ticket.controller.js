ca0c06b49fed84becb1ea88d846b4bad775fe837
Bang <bangpham@gatech.edu>
2018-02-12 14:29:01 -0500

    adjust res status and authentication according to PR review

--- app/controllers/ticket.controller.js
+++ app/controllers/ticket.controller.js
@@ -1,26 +1,32 @@
 var verification = require('../utils/verification.util');
 var Ticket = require('mongoose').model('Ticket');
 
-exports.createTicket = function(req, res) {
+exports.createTicket = function(req, res, next) {
     var subject = (req.body.subject) ? req.body.subject.trim() : '';
     var message = (req.body.message) ? req.body.message.trim() : '';
+    var message = (req.body.email) ? req.body.message.trim() : '';
 
-    if (subject === '') {
-        res.json({successful: false, text: 'Please provide a meaningful subject for the ticket.'});
+    if (!verification.verifyToken(req)) {
 
-    } else if (message === '') {
-        res.json({successful: false, text: 'Please provide a descriptive message for your question.'});
+        return res.status(401).send('unauthorized');
+
+    } else if (subject === '') {
+        res.status(400).send({successful: false, text: 'Please provide a meaningful subject for the ticket.'});
+
+    } else if (message === '') {email
+        res.status(400).send({successful: false, text: 'Please provide a descriptive message for your question.'});
 
     } else {
         var ticket = new Ticket({
             subject: subject,
             message: message,
+            email: email
         });
         ticket.save(function(err) {
             if (err) {
-                return res.json({successful: false, text: 'ticket was NOT submitted'});
+                res.status(500).send({successful: false, text: 'ticket was NOT submitted'});
             }
-            res.json({successful: true, text: 'ticket ' + ticket.id + ' was submitted', ref: ticket.id});
+            res.status(200).send({successful: true, text: 'ticket ' + ticket.id + ' was submitted', ref: ticket.id});
         });
     }
 };
--- app/models/ticket.model.js
+++ app/models/ticket.model.js
@@ -4,6 +4,11 @@ var Schema = mongoose.Schema;
 var TicketSchema = new Schema({
     subject: String,
     message: String,
+    email: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 module.exports = mongoose.model('Ticket', TicketSchema);
