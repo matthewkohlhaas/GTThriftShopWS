ec05ce284342c8bf1db18cf09bea96f7fc9a2b0d
amalladi3 <adu1000@gmail.com>
2018-02-14 18:05:53 -0500

    backend create listing dynamically

--- app/controllers/listings.controller.js
+++ app/controllers/listings.controller.js
@@ -14,3 +14,40 @@ exports.list = function (req, res, next) {
         }).populate('user');
     }
 };
+
+exports.createListing = function(req, res, next) {
+    var name = (req.body.name) ? req.body.name.trim() : '';
+    var price = (req.body.message) ? req.body.price.trim() : '';
+    var description = (req.body.description) ? req.body.description.trim() : '';
+    var imageUrl = (req.body.imageUrl) ? req.body.imageUrl.trim() : '';
+    console.log(description);
+    var user = authentication.getUserFromToken(req);
+
+    //authenticate
+    if (!user) {
+        res.status(401).send('unauthorized');
+    } else if (name === '') {
+        res.status(400).send({successful: false, text: 'Please provide a name.'});
+    } else {
+        new Listing({
+            name: name,
+            description: description,
+            price: price,
+            imageUrl: imageUrl,
+            user: user._id
+
+        }).save(function(err) {
+            if (err) {
+                console.log('fail');
+                res.status(500).send({successful: false, text: 'Failed to create listing:.' + name});
+            } else {
+                console.log('success');
+                res.status(200).send({successful: true, text: 'Created listing:' + name + '!'});
+            }
+        });
+    }
+};
+
+exports.getById = function(req, res, next) {
+
+}
