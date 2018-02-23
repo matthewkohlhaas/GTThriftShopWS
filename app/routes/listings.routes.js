098cadd40d561b62f9b4fe6c5c32376c5b812b00
Scott Wolfe <swolfe8@gatech.edu>
2018-02-23 09:54:27 -0500

    use auth middelware for listings

--- app/routes/listings.routes.js
+++ app/routes/listings.routes.js
@@ -1,10 +1,12 @@
 var listings = require('../controllers/listings.controller');
+var auth = require('../utils/auth-middleware.utils');
 
 module.exports = function (app) {
-    app.get('/listings', listings.list);
+    app.get('/listings',
+        auth.authenticateTokenMiddleware,
+        listings.list);
 
     app.post('/create-listing', listings.createListing);
 
     app.route('/get-listing-by-id').get(listings.getById);
-
 };
