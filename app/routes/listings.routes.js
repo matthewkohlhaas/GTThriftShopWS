f406ddbefc68f186877c9d751ba61bf030cdd777
Scott Wolfe <swolfe8@gatech.edu>
2018-02-23 09:47:59 -0500

    refactor to match

--- app/routes/listings.routes.js
+++ app/routes/listings.routes.js
@@ -1,7 +1,10 @@
 var listings = require('../controllers/listings.controller');
 
 module.exports = function (app) {
-    app.route('/listings').get(listings.list);
+    app.get('/listings', listings.list);
+
     app.post('/create-listing', listings.createListing);
+
     app.route('/get-listing-by-id').get(listings.getById);
+
 };
