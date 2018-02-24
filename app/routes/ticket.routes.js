440c993437fe9e59673187db49b174e47a755e71
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-02-24 08:09:11 -0500

    Refactor ticket routes

--- app/routes/ticket.routes.js
+++ app/routes/ticket.routes.js
@@ -1,5 +1,5 @@
 var ticket = require('../controllers/ticket.controller');
 
 module.exports = function(app) {
-    app.post('/create-ticket', ticket.createTicket);
+    app.post('/tickets', ticket.createTicket);
 };
