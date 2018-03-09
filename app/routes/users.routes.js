ef74915785f559961ce5b212a041c617a93187f2
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-03-09 04:47:11 -0500

    Clean up user routes

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -3,15 +3,13 @@ var admins = require('../controllers/admins.controller');
 var auth = require('../utils/auth-middleware.utils');
 
 module.exports = function (app) {
-    app.route('/users').post(users.createAccount);
+    app.post('/users', users.createAccount);
 
     app.get('/users/:id',
         auth.authenticateTokenMiddleware,
         users.getUserFromId
     );
 
-    // TODO add a PUT at this route to update currently logged in user
-    // So, you change it to app.route('/users/from-token').get(users.getUserFromToken).put(users.someFunctionName);
     app.get('/users/from-token', users.getUserFromToken);
   
     app.post('/user/update-first-name', users.updateFirstName);
@@ -21,9 +19,6 @@ module.exports = function (app) {
     app.post('/user/update-profile-picture-url', users.updateProfilePictureUrl);
 
     app.post('/user/update-profile-bio', users.updateProfileBio);
-
-    // TODO use this route later
-    // app.get('/users/:email', users.getUserByEmail);
 
     app.post('/users/send-verification', users.resendVerificationEmail);
 
