03eb27729e1d880ff3ce8437c198b8fed4b17308
Scott Wolfe <swolfe8@gatech.edu>
2018-03-26 13:04:48 -0400

    move listing param to beginning of list

--- app/routes/messages.routes.js
+++ app/routes/messages.routes.js
@@ -2,7 +2,7 @@ var messages = require('../controllers/messages.controller');
 var auth = require('../utils/auth-middleware.utils');
 
 module.exports = function (app) {
-    app.get('/messages/:first_user_id/:second_user_id/:listing_id',
+    app.get('/messages/:listing_id/:first_user_id/:second_user_id',
         auth.authenticateTokenMiddleware,
         messages.findMessages
     );
