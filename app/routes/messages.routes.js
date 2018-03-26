950afac284ac13aaf971274b048d1db4a00272db
Scott Wolfe <swolfe8@gatech.edu>
2018-03-26 11:08:20 -0400

    add Message routes

---
+++ app/routes/messages.routes.js
@@ -0,0 +1,14 @@
+var listings = require('../controllers/messages.controller');
+var auth = require('../utils/auth-middleware.utils');
+
+module.exports = function (app) {
+    app.get('/messages/:first_user_id/:second_user_id/:listing_id',
+        // auth.authenticateTokenMiddleware,
+        // do something
+    );
+
+    app.post('/messages',
+        // auth.authenticateTokenMiddleware,
+        // do something
+    );
+};
