4dd98ef771c77f5097d2065eac4397a43a8bf3e1
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-04-15 02:38:45 -0400

    Fix route formatting for listings

--- app/routes/listings.routes.js
+++ app/routes/listings.routes.js
@@ -2,23 +2,25 @@ var listings = require('../controllers/listings.controller');
 var auth = require('../utils/auth-middleware.utils');
 
 module.exports = function (app) {
-    app.route('/listings/:id')
-        .get(
-            auth.authenticateToken,
-            listings.getById
-        ).put(
-            auth.authenticateToken,
-            listings.editListing
-        );
+    app.route(
+        '/listings/:id'
+    ).get(
+        auth.authenticateToken,
+        listings.getById
+    ).put(
+        auth.authenticateToken,
+        listings.editListing
+    );
 
-    app.route('/listings')
-        .get(
-            auth.authenticateToken,
-            listings.list,
-            listings.postProcessListings
-        ).post(
-            listings.createListing
-        );
+    app.route(
+        '/listings'
+    ).get(
+        auth.authenticateToken,
+        listings.list,
+        listings.postProcessListings
+    ).post(
+        listings.createListing
+    );
 
     app.get('/listings/users/:userId',
         auth.authenticateToken,
